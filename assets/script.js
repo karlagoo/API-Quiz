1c53e979b4cead213fc335d4896efbcd2a0041dc
Karla Goo <karla_goo@yahoo.com>
2021-06-27 10:34:00 -0500

    added initial functions

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,41 @@
 // user clicks on start button to begin game
-var generateBtn = document.querySelector("#button");
-var feedback = document.getElementById("feedback")
-var quizQuestionIndex = 0
+
+//declaring the variables
+var startBtn = document.querySelector("#button");
+var feedback = document.getElementById("feedback");
+var answerOptions = document.getElementById("answerOptions");
+var questionTitle = document.getElementById("questionTitle");
+
+
+
+
+
+var quizQuestionIndex = 0;
+
+
 var questions = [
     {
         question: "What does API stand for?",
-        answer1: "Application Programming Interface",
-        answer2: "Algorithm Programming Involvement",
-        answer3: "Alocating Popular interfaces",
-        answer4: "Approrpriating Programming Interface",
-        correctAnswer: "Application Programming Interface",
-    },
-    {
-        question: "This is a question",
-        answer1: "Application Programming Interface",
-        answer2: "Algorithm Programming Involvement",
-        answer3: "Alocating Popular interfaces",
-        answer4: "Approrpriating Programming Interface",
+        answerOptions: ["Application Programming Interface","Application Programming Interface","Alocating Popular interfaces","Approrpriating Programming Interface"],
         correctAnswer: "Application Programming Interface",
     },
     {
         question: "What does API stand for?",
-        answer1: "Application Programming Interface",
-        answer2: "Algorithm Programming Involvement",
-        answer3: "Alocating Popular interfaces",
-        answer4: "Approrpriating Programming Interface",
+        answerOptions: ["Application Programming Interface","Application Programming Interface","Alocating Popular interfaces","Approrpriating Programming Interface"],
         correctAnswer: "Application Programming Interface",
-    }
+    },
     {
         question: "What does API stand for?",
-        answer1: "Application Programming Interface",
-        answer2: "Algorithm Programming Involvement",
-        answer3: "Alocating Popular interfaces",
-        answer4: "Approrpriating Programming Interface",
+        answerOptions: ["Application Programming Interface","Application Programming Interface","Alocating Popular interfaces","Approrpriating Programming Interface"],
+        correctAnswer: "Application Programming Interface",
+    },
+    {
+        question: "What does API stand for?",
+        answerOptions: ["Application Programming Interface","Application Programming Interface","Alocating Popular interfaces","Approrpriating Programming Interface"],
         correctAnswer: "Application Programming Interface",
     }
 ]
+
 // start quiz function goes here - here we start timer, and show the starting time. Hide start screen and unhide the quiz quesions
 function startQuiz () {
     quizQuestion ();
@@ -63,5 +63,5 @@ function rightAnswer (){
 // when game is over initials with score are stored
 // Quiz end function - stop timer, show high score screen, hide questions, if timer runs out call this function
 function gameOver () {
-
 }
+// startBtn.onclick = startQuiz
