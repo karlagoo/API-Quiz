47b09fdfdc500d0bcc8613997aff1b0f3b80e296
Karla Goo <karla_goo@yahoo.com>
2021-06-28 23:01:52 -0500

    created option functions

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 // user clicks on start button to begin game
 
 //declaring the variables
-var startBtn = document.querySelector("#button");
+var startBtn = document.querySelector("#startBtn");
 var feedback = document.getElementById("feedback");
 var answerOptions = document.getElementById("answerOptions");
-var questionTitle = document.getElementById("questionTitle");
+var questionTitle = document.getElementById("question-title");
 
 
 
@@ -20,9 +20,9 @@ var questions = [
         correctAnswer: "Application Programming Interface",
     },
     {
-        question: "What does API stand for?",
-        answerOptions: ["Application Programming Interface","Application Programming Interface","Alocating Popular interfaces","Approrpriating Programming Interface"],
-        correctAnswer: "Application Programming Interface",
+        question: "What do we use to call a function?",
+        answerOptions: ["()","[]","{}","<>"],
+        correctAnswer: "()",
     },
     {
         question: "What does API stand for?",
@@ -39,16 +39,31 @@ var questions = [
 // start quiz function goes here - here we start timer, and show the starting time. Hide start screen and unhide the quiz quesions
 function startQuiz () {
     quizQuestion ();
+    console.log("startBtn clicked")
 }
 
 
 // this function displays the next questions - gets questions from questions array
 function quizQuestion (){
-    var quizQuestion = questions[quizQuestionIndex];
+    var quizQuestion = questions[quizQuestionIndex].question;
+    var answerOptions = questions[quizQuestionIndex].answerOptions;
+    startBtn.style.display = "none"
+    document.getElementById("question-title").innerHTML = quizQuestion;
+    //document.getElementById("answer-options").innerHTML = answerOptions;
+    for(var i = 0; i < answerOptions.length; i++){
+        //create a button for each option
+        var optionBtn = document.createElement("button");
+        //display option value on each button
+        optionBtn.innerHTML = answerOptions[i];
+        document.getElementById("answer-options").appendChild(optionBtn)
+    }
+
 }
 
 // create a function for wrong click and right click - this one is right click
 function rightAnswer (){
+    quizQuestionIndex++;
+    quizQuestion();
     if (this.val === questions[quizQuestionIndex].correctAnswer){
         feedback.textContent = "Correct!";
     } else {
@@ -64,4 +79,4 @@ function rightAnswer (){
 // Quiz end function - stop timer, show high score screen, hide questions, if timer runs out call this function
 function gameOver () {
 }
-// startBtn.onclick = startQuiz
+startBtn.onclick = startQuiz
